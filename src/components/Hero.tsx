c02cbcff6c7a88cf803cadabb7a15757ebd9d080
tomaCezar1 <cezar.toma1@gmail.com>
2026-02-18 19:50:01 +0200

    add new gsap timeline animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,6 +27,18 @@ const Hero = () => {
       stagger: 0.06,
       delay: 1,
     });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: '#hero',
+          start: 'top top',
+          end: 'bottom top',
+          scrub: true,
+        },
+      })
+      .to('.right-leaf', { y: 200 }, 0)
+      .to('.left-leaf', { y: -200 }, 0);
   }, []);
 
   return (
