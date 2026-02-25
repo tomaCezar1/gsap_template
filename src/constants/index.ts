ad7259271df775cea7a0b683f94a1640ebf84892
tomaCezar1 <cezar.toma1@gmail.com>
2026-02-25 14:17:26 +0200

    new section in the page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,3 +16,30 @@ export const navLinks = [
     title: 'Contact',
   },
 ];
+
+export const cocktailsList = [
+  {
+    name: 'Chapel Hill Shiraz',
+    country: 'AU',
+    detail: 'Battle',
+    price: '100 MDL',
+  },
+  {
+    name: 'Caten Malbee',
+    country: 'AU',
+    detail: 'Battle',
+    price: '149 MDL',
+  },
+  {
+    name: 'Rhino Pale Ale',
+    country: 'CA',
+    detail: '750 ml',
+    price: '120 MDL',
+  },
+  {
+    name: 'Irish Guinness',
+    country: 'IE',
+    detail: '600 ml',
+    price: '190 MDL',
+  },
+];
