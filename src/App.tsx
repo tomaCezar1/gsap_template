d0c248917461fd25a9c00fff098776596739ac3c
tomaCezar1 <cezar.toma1@gmail.com>
2026-02-25 15:50:19 +0200

    new components to added App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,7 +12,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
+      <About />
     </main>
   );
 };
