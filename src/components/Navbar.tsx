0e3d3b3b93e6e11f61c75f27ee7386f9f81d1c26
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-03-23 19:07:12 +0200

    some more updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       <div>
         <a href="#home" className="flex items-center gap-2">
           <img
-            src="/images/logo.png"
+            src="images/logo.png"
             alt="logo"
             className="w-8 h-8 object-contain"
           />
